da012b699c5f0d9c605cbb84b72aab1459ee90fa
unknown <tomygarnero9429@gmail.com>
2023-02-09 17:50:01 -0300

    Show query results

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ function mostrarAlerta(mensaje) {
 function buscarImagenes(termino) {
     
     const key = "33515490-1d720cad3d96e05847956fbb0";
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
     // console.log(url);
 
     fetch(url)
@@ -52,5 +52,29 @@ function buscarImagenes(termino) {
 }
 
 function mostrarImagenes(imagenes) {
-    console.log(imagenes);
+    // console.log(imagenes);
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // Interar sobre el arreglo de imagenes y construir el HTML
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 mb-4 p-3">
+                <div class="bg-white ">
+                    <img class="w-full" src=${previewURL} alt={tags} />
+                    <div class="p-4">
+                        <p class="card-text">${likes} Me Gusta</p>
+                        <p class="card-text">${views} Vistas </p>
+        
+                        <a href=${largeImageURL} 
+                        rel="noopener noreferrer" 
+                        target="_blank" class="bg-blue-800 w-full p-1 block mt-5 rounded text-center font-bold uppercase hover:bg-blue-500 text-white">Ver Imagen</a>
+                    </div>
+                </div>
+            </div>
+            `;
+    })
 }  
