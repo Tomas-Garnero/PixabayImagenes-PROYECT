ec79ac1ff25c4f7a195d72f2586191e65c81f04f
unknown <tomygarnero9429@gmail.com>
2023-02-10 13:34:54 -0300

    Some files changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const resultado = document.querySelector("#resultado");
-const formulario = document.querySelector("#formulario");
 const paginacionDiv = document.querySelector("#paginacion");
+
+const formulario = document.querySelector("#formulario");
 
 const registrosPorPagina = 40;
 let totalPaginas;
@@ -20,7 +21,7 @@ function validarFormulario(e) {
         return;
     }
 
-    buscarImagenes(terminoBusqueda);
+    buscarImagenes();
 }
 
 function mostrarAlerta(mensaje) {
@@ -32,7 +33,7 @@ function mostrarAlerta(mensaje) {
         alerta.classList.add("bg-red-100", "border-red-400", "text-red-700", "px-4", "py-3", "rounded", "max-w-lg", "mx-auto", "mt-6", "text-center");
 
         alerta.innerHTML = `
-            <strong class="font-bold">Error</strong>
+            <strong class="font-bold">Error!</strong>
             <span class="block sm:inline">${mensaje}</span>
         `;
 
